d9f33c957eaf1908354b27d2af08f51600844583
StanWarford <Stan.Warford@pepperdine.edu>
2017-06-15 09:14:23 -0700

    Counterexample program for Ordering

--- WaitOrderingCounterexample/main.c
+++ WaitOrderingCounterexample/main.c
@@ -2,17 +2,17 @@
  * File:   main.c
  * Author: warford
  *
- * Created on June 14, 2017, 2:28 PM
+ * Created on June 14, 2017, 7:50 PM
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 
 /*
- * Program to test Wait ordering theorem.
+ * Program to generate Wait Ordering counterexample
  */
 
-const int numStates = 8;
+const int numStates = 7;
 
 int p[numStates];
 int q[numStates];
@@ -32,6 +32,17 @@ void printStates(int *s) {
         printf("%d ", s[i]);
     }
     printf("\n");
+}
+
+void nextState(int *s) {
+    int carryIn = (s[numStates - 1] + 1) / 2;
+    int carryOut;
+    s[numStates - 1] = (s[numStates - 1] + 1) % 2;
+    for (int j = numStates - 2; 0 <= j; j--) {
+        carryOut = (s[j] + carryIn) / 2;
+        s[j] = (s[j] + carryIn) % 2;
+        carryIn = carryOut; // Re-establish loop invariant for next column
+    }
 }
 
 void not(int *first, int *result) {
@@ -104,50 +115,46 @@ void myWait(int *first, int *second, int *result) {
 }
 
 int main(int argc, char** argv) {
-    p[0] = 0;
-    p[1] = 0;
-    p[2] = 1;
-    p[3] = 1;
-    p[4] = 1;
-    p[5] = 1;
-    p[6] = 1;
-    p[7] = 1;
-    q[0] = 0;
-    q[1] = 1;
-    q[2] = 0;
-    q[3] = 1;
-    q[4] = 1;
-    q[5] = 1;
-    q[6] = 1;
-    q[7] = 1;
-    r[0] = 0;
-    r[1] = 0;
-    r[2] = 0;
-    r[3] = 1;
-    r[4] = 1;
-    r[5] = 1;
-    r[6] = 1;
-    r[7] = 1;
-    myWait(p, q, p_wait_q);
-    not(q, not_q);
-    myWait(not_q, r, not_q_wait_r);
-    and(p_wait_q, not_q_wait_r, p_wait_q_and_not_q_wait_r);
-    myWait(p, r, p_wait_r);
-    printf("                        p: ");
-    printStates(p);
-    printf("                        q: ");
-    printStates(q);
-    printf("                        r: ");
-    printStates(r);
-    printf("                 p Wait q: ");
-    printStates(p_wait_q);
-    printf("                    Not q: ");
-    printStates(not_q);
-    printf("             Not q Wait r: ");
-    printStates(not_q_wait_r);
-    printf("p Wait q And Not q Wait r: ");
-    printStates(p_wait_q_and_not_q_wait_r);
-    printf("                 p Wait r: ");
-    printStates(p_wait_r);
+    int numStates2 = 1 << numStates;
+    printf("numStates == %d, numStates2 == %d\n\n", numStates, numStates2);
+    for (int i = 0; i < numStates; i++) {
+        p[i] = 0;
+        q[i] = 0;
+        r[i] = 0;
+    }
+    for (int i = 0; i < numStates2; i++) {
+        for (int j = 0; j < numStates2; j++) {
+            for (int k = 0; k < numStates2; k++) {
+                for (int m = 0; m < numStates; m++) {
+                    myWait(p, q, p_wait_q);
+                    not(q, not_q);
+                    myWait(not_q, r, not_q_wait_r);
+                    and(p_wait_q, not_q_wait_r, p_wait_q_and_not_q_wait_r);
+                    myWait(p, r, p_wait_r);
+                    if (p_wait_q_and_not_q_wait_r[m] == 1 && p_wait_r[m] == 0) {
+                        printf("                        p: ");
+                        printStates(p);
+                        printf("                        q: ");
+                        printStates(q);
+                        printf("                        r: ");
+                        printStates(r);
+                        printf("                 p Wait q: ");
+                        printStates(p_wait_q);
+                        printf("                    Not q: ");
+                        printStates(not_q);
+                        printf("             Not q Wait r: ");
+                        printStates(not_q_wait_r);
+                        printf("p Wait q And Not q Wait r: ");
+                        printStates(p_wait_q_and_not_q_wait_r);
+                        printf("                 p Wait r: ");
+                        printStates(p_wait_r);
+                    }
+                }
+                nextState(r);
+            }
+            nextState(q);
+        }
+        nextState(p);
+    }
     return (EXIT_SUCCESS);
 }
